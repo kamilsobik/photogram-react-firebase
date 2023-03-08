eb8758097b32d9edcbd3ecd39fbdaff96c6e37be
Kamil Sobik <kamil.sobik@gmail.com>
2023-03-08 15:16:47 +0100

    secure key for firebase by add package react-dotenv

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import env from "react-dotenv";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIRABASE_API_KEY,
+  authDomain: "kamil-photogram.firebaseapp.com",
+  projectId: "kamil-photogram",
+  storageBucket: "kamil-photogram.appspot.com",
+  messagingSenderId: "303080540042",
+  appId: process.env.REACT_APP_FIRABASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const projectStorage = getStorage();
+const projectFirestore = getFirestore();
+
+export { projectStorage, projectFirestore };
